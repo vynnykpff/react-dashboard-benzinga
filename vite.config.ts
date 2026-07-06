84ae9f3c51e64d50a32de743ca3d47588dfb4bc4
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2026-07-07 01:00:25 +0300

    chore: Install UI dependencies and adjust app structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
 })
